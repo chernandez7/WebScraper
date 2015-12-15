c8de9d77eb1ca599a8f863346ca08b0edb4869c4
chernandez7 <chernandez7@luc.edu>
2015-12-15 02:40:22 -0600

    Formatting changes and optimizations

--- Parser.py
+++ Parser.py
@@ -9,13 +9,12 @@ import requests
 #Initialization of variables
 start = time.time()
 lock = threading.Lock()
-number_lock = threading.Lock()
 url_queue = Queue(100)
 item_count = 0
 
 #Creating and opening file
 file = open("data.txt", "w+")
-file.write("{:40} {:15} {:14}\n".format("Name", "Count", "Price"))
+file.write("{:59} {:19} {:18}\n".format("Name", "Count", "Price"))
 
 #Worker class that threads run
 def worker():
@@ -44,28 +43,26 @@ def worker():
             safe_write_to_file(list_of_cells)
         url_queue.task_done()
 
-#Thread-Safe form of writing to file
+#Thread-Safe form of writing to file and formatting
 def safe_write_to_file(input):
     lock.acquire()
-    file.write("\t".join(map(str, input)).strip()+ "\n")
+    file.write("{:60}{:20}{:20}\n".format(*(str(i).strip() for i in input)))
     lock.release()
 
 #Thread-Safe iteration of a number
 def safe_iterate(number):
-    number_lock.acquire()
     number += 1
-    number_lock.release()
     return number
 
 #Creating a pool of workers
-for i in range(10):
+for i in range(15):
     t = Thread(target = worker)
     t.daemon = True
     t.start()
         
 #4345 Pages to run in the site
-urllist = ["http://ragial.com/search/iRO-Renewal//{}".format(str(pgnum)) for pgnum in range(1, 4345+1)]
-#urllist = ["http://ragial.com/search/iRO-Renewal//{}".format(str(pgnum)) for pgnum in range(1, 100+1)]
+#urllist = ["http://ragial.com/search/iRO-Renewal//{}".format(str(pgnum)) for pgnum in range(1, 4345+1)]
+urllist = ["http://ragial.com/search/iRO-Renewal//{}".format(str(pgnum)) for pgnum in range(1, 10+1)]
 for url in urllist:
     url_queue.put(url)
 
