59c05292ee67517124b03bdb7e49f4c3f43e6161
chernandez7 <chernandez7@luc.edu>
2015-12-14 22:15:10 -0600

    Scraper works and formatted

--- Parser.py
+++ Parser.py
@@ -6,38 +6,33 @@ from bs4 import BeautifulSoup
 start = time.time()
 
 #4345 Pages to run in the site
-#url = ["http://ragial.com/search/iRO-Renewal//{}".format(str(pgnum)) for pgnum in range(1, 4345+1)]
+urllist = ["http://ragial.com/search/iRO-Renewal//{}".format(str(pgnum)) for pgnum in range(1, 4345+1)]
 #print ("\n".join(url))
 
 #for loop start
+for url in urllist:
+    print ("Processing {}...".format(url))
+    response = requests.get(url)
+    html = response.content
 
-url = "http://ragial.com/search/iRO-Renewal//"
-print ("Processing {}...".format(url))
-response = requests.get(url)
-html = response.content
+    soup = BeautifulSoup(html, "html.parser")
 
-soup = BeautifulSoup(html, "html.parser")
+    table = soup.find("table") #no attrs needed because of ragial format
 
-table = soup.find("table") #no attrs needed because of ragial format
+    list_of_rows = []
+    for row in table.findAll("tr")[1:]:
+        list_of_cells = []
+        for cell in row.findAll("td"):
+            for a in cell.findAll("td"):
+                list_of_cells.append(name)
+            list_of_cells.append(cell.text)
+        list_of_rows.append(list_of_cells)
 
-list_of_rows = []
-for row in table.findAll("tr")[1:]:
-    list_of_cells = []
-    for cell in row.findAll("td"):
-        for a in cell.findAll("td"):
-            name = cell.text.strip(" ")
-            index = name.find("x")
-            name = name[:index]
-            list_of_cells.append(name)
-        list_of_cells.append(cell.text)
-    list_of_rows.append(list_of_cells)
-print (list_of_rows)
+file = open("data.txt", "w+")
+file.write("{:40} {:15} {:14}\n".format("Name", "Count", "Price"))
+for list_of_cells in list_of_rows:
+    file.write("\t".join(map(str, list_of_cells)).strip()+ "\n")
 
-
-#outfile = open("./parser.csv", "wb")
-#writer = csv.writer(outfile)
-#writer.writerow({"Name", "Count", "Price", "Discount?"})
-#writer.writerows(list_of_rows)
-
+file.close()
 end = time.time()
 print ("Finished in {} seconds".format(str(end - start)))
